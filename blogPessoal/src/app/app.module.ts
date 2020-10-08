3ab92ca7ab9fd7ab469f7a78355913cf5f33d99e
Brunostelzeneder <52729196+Brunostelzeneder@users.noreply.github.com>
2020-10-08 11:04:21 -0300

    atualizacao postagem-tema

--- blogPessoal/src/app/app.module.ts
+++ blogPessoal/src/app/app.module.ts
@@ -1,9 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http'
-import { FormsModule } from '@angular/forms';
-
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +12,9 @@ import { FeedComponent } from './feed/feed.component';
 import { PerfilLateralComponent } from './perfil-lateral/perfil-lateral.component';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { FormsModule } from '@angular/forms';
+import { OrderModule } from 'ngx-order-pipe';
+import { PostTemaComponent } from './post-tema/post-tema.component';
 
 
 @NgModule({
@@ -27,7 +27,8 @@ import { CadastroComponent } from './cadastro/cadastro.component';
     PerfilLateralComponent,
     LoginComponent,
     CadastroComponent,
-
+    PostTemaComponent,
+    
   ],
   imports: [
     BrowserModule,
@@ -35,7 +36,7 @@ import { CadastroComponent } from './cadastro/cadastro.component';
     FontAwesomeModule,
     HttpClientModule,
     FormsModule,
-
+    OrderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
